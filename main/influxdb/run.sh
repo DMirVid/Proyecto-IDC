b639ba94b253dc494350cfdfdcd52c0f6f175c29
Mairon243 <126103086+DMirVid@users.noreply.github.com>
2025-06-07 21:11:09 +0200

    Error al inicalizar influxdb

--- main/influxdb/run.sh
+++ main/influxdb/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+#No se conecta a inlfux
 influx
 CREATE DATABASE telegraf
 CREATE USER telegraf WITH PASSWORD 'uforobot'
 GRANT ALL ON telegraf TO telegraf
 CREATE RETENTION POLICY thirty_days ON telegraf DURATION 30d REPLICATION 1 DEFAULT
+exit
 echo Base de datos creada correctamente
 exit
