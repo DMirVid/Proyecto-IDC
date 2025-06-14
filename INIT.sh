c4fa0a23761bfa60dcbcdc6c6f8ca6102709c229
Mairon243 <126103086+DMirVid@users.noreply.github.com>
2025-06-15 00:23:00 +0200

    Add - Script que inicia todos los servicios. Revisar la web de grafana

---
+++ INIT.sh
@@ -0,0 +1,14 @@
+echo Eliminando instancias anteriores
+sudo docker rm -f $(sudo docker ps -aq)
+
+echo Iniciando contenedores
+sudo docker compose -f ./main/docker-compose.yml up -d
+for i in {1..10}
+do 
+    echo -n .
+    sleep 0.2
+done
+echo .
+sudo docker exec influxdb ./run.sh
+sudo docker edxec influxdb influx -execute "SHOW USERS"
+echo Puede visualizar los datos en http://localhost:3000
